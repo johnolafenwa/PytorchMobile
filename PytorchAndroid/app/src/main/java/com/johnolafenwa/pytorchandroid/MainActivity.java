08b905da5efed278557eb8373406eea827061005
Satish Gaurav <satishgaurav@summer.local>
2021-04-26 00:47:07 +0530

    update old pytorch androidx dependeny

--- PytorchAndroid/app/src/main/java/com/johnolafenwa/pytorchandroid/MainActivity.java
+++ PytorchAndroid/app/src/main/java/com/johnolafenwa/pytorchandroid/MainActivity.java
@@ -4,11 +4,14 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.provider.MediaStore;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
 import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
@@ -46,18 +49,19 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
 
-        if(requestCode == cameraRequestCode && resultCode == RESULT_OK){
+        if (requestCode == cameraRequestCode && resultCode == RESULT_OK) {
 
-            Intent resultView = new Intent(this,Result.class);
+            Intent resultView = new Intent(this, Result.class);
 
-            resultView.putExtra("imagedata",data.getExtras());
+            resultView.putExtra("imagedata", data.getExtras());
 
             Bitmap imageBitmap = (Bitmap) data.getExtras().get("data");
 
             String pred = classifier.predict(imageBitmap);
-            resultView.putExtra("pred",pred);
+            resultView.putExtra("pred", pred);
 
             startActivity(resultView);
 
--- PytorchAndroid/app/src/main/java/com/johnolafenwa/pytorchandroid/Result.java
+++ PytorchAndroid/app/src/main/java/com/johnolafenwa/pytorchandroid/Result.java
@@ -2,10 +2,12 @@ package com.johnolafenwa.pytorchandroid;
 
 import android.graphics.Bitmap;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 public class Result extends AppCompatActivity {
 
